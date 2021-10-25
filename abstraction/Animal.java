3aedc354e22bd7e918539d7867231c9277daf645
Sharada R <56782190+SharadaRaghu@users.noreply.github.com>
2021-10-25 10:27:31 -0700

    Add files via upload

---
+++ abstraction/Animal.java
@@ -0,0 +1,12 @@
+package com.abstraction;
+
+abstract public class Animal {
+	abstract void animalcolor();
+	abstract void animalsound();
+	abstract void animaleating();
+	
+	public void animalSleep()
+	{
+		System.out.println("The animal sleeps");
+	}
+}
